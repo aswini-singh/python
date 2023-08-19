28f19270967a2da47355ca02dc6f637b9726c492
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-19 19:42:42 +0530

    new changes

--- DataTypes/complex.py
+++
@@ -1,6 +0,0 @@
-a = 5+2j
-print(a)
-print(type(a))
-
-b = 2j
-print(type(b))
---
+++ DataTypes/sclar datatype/complex.py
@@ -0,0 +1,11 @@
+a = 5+2j
+print(a)
+print(type(a))
+
+b = 2j
+print(type(b))
+
+a = 3+2j
+b= 6+8j
+print(a+b)
+print(a*b)

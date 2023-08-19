4dfce3e7eb4db8e2594fb065618b61d22157b271
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-19 15:08:24 +0530

    adding datatypes

---
+++ DataTypes/datatypes.py
@@ -0,0 +1,34 @@
+a = 10
+print(type(a)) 
+
+a = 10.5
+print(type(a)) 
+
+a = 2+4j
+print(type(a)) 
+
+a='20'
+print(type(a)) 
+
+a = True
+print(type(a)) 
+
+a = [1,2]
+print(type(a)) 
+
+a=(1,2)
+print(type(a)) 
+
+a=1,2,
+print(type(a)) 
+
+a={1,2}
+print(type(a)) 
+
+a={}
+print(type(a)) 
+
+a = None
+print(type(a)) 
+
+

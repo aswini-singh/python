987f8604dde3cf8bb0b8b862b6464a5d3cd6d936
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-19 19:41:55 +0530

    new code

---
+++ DataTypes/sclar datatype/int.py
@@ -0,0 +1,37 @@
+x = 1_234_567_890
+print(x)
+print(type(x))
+
+y = int('-10')
+print(y)
+print(type(y))
+
+z = int(5.5)
+print(z)
+print(type(z))
+
+
+m=int('100')  # base '0'
+print(m)
+print(type(m))
+
+l=int('100',6)
+print(l)
+print(type(l))
+
+n = int('100', 2)  
+print(n)
+print(type(n))
+
+
+a = -1273210
+print(a)
+print(type(a))
+
+a = 767787657667855656571273210
+print(a)
+print(type(a))
+
+
+
+

9bb19fd666503db62d0d84cc2015455da3b4b764
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-19 15:48:17 +0530

    new code

---
+++ DataTypes/float.py
@@ -0,0 +1,20 @@
+a = -1273210.87
+print(a)
+print(type(a))
+
+a = 5032563676372183210.0
+print(a)
+print(type(a))
+
+f = 1e3
+print(f)
+
+f = 3.4556789e2
+print(f) #output:
+print(type(f))
+
+f=float('5')
+print(f)
+
+f=float('   5')
+print(f)
